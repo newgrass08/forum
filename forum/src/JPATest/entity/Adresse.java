9603c1ebed2804c569515c23a5c41f923158bb82
newgrass08 <adrien.salvatico@gmail.com>
2017-04-03 15:08:05 +0200

    first

---
+++ forum/src/JPATest/entity/Adresse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JPATest.entity;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author admin
+ */
+@Embeddable
+public class Adresse {
+    private String rue;
+    private Integer numero;
+    private Integer codePostal;
+    private String ville;
+    private String pays; 
+}
